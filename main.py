2d5fa8863e4de338dcda2a5e4b2940536287b317
KaystroAnton <anton.kaystro@mail.ru>
2025-05-21 12:11:59 +0300

    add

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import Constants
 flag = "Simulation"
 #flag = "Real"
 for control in range(3):
-    test1 = test.Test(flag,controlFuncAndParam= Constants.controlFuncAndParamList[2])
+    test1 = test.Test(flag,targetPos=None,refAng= None,standParam=None,dt = 1/240,maxTime =10,
+                      controlFuncAndParam= Constants.controlFuncAndParamList[2],numberOfTests = 1)
     test1.run()
 # for info use __docs__
 #print(test.createParamListOfControl.__doc__)
